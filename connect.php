a8a290883093d1064a4be43ac4593631b42b59d6
Timmy Ocampo <146462276+chichidogie@users.noreply.github.com>
2023-11-24 09:32:18 +0800

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php
+// Database connection configuration
+$connection = mysqli_connect('localhost', 'root', '', 'db_softeng');
+
+
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL Server".
+    mysqli_connect_errno();   
+} else{
+    echo "Francine Timmy Ocampo ";
+}
+?>
